31a16414540fbccde11909ed5735c84223dde0c3
Efim <elavrukhin@ozon.ru>
2020-02-20 12:11:04 +0300

    fix update interval again

--- stack.py
+++ stack.py
@@ -127,7 +127,7 @@ class Stack:
         model.eval()
         with torch.no_grad():
             offset = 0
-            for item in tqdm(dataloader,  miniters=len(dataloader)//100):
+            for item in tqdm(dataloader, mininterval=10, maxinterval=20):
 
                 def handle_batch():
                     if isinstance(item, tuple):
